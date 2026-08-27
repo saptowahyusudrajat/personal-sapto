3c7f3fd4f368bc79802303867b17d9cabf02a720
saptowahyusudrajat <mtvgsapto@gmail.com>
2026-08-27 13:57:10 +0700

    fix last updated and null

--- src/app/api/sensor/route.ts
+++ src/app/api/sensor/route.ts
@@ -77,7 +77,7 @@ export async function GET(request: NextRequest) {
       query = query.gte('timestamp', pastTime);
     }
 
-    query = query.order('timestamp', { ascending: true });
+    query = query.order('timestamp', { ascending: true }).range(0, 100000);
 
     const { data, error } = await query;
 
